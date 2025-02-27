a1327a044e77e91d98832a20c8e0775c84777a25
KindImagination <74197848+KindImagination@users.noreply.github.com>
2025-02-27 19:08:29 +0100

    bugfix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,6 +83,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiHost: process.env.API_HOST || 'localhost:3000',
-    }
+    },
   },
 })
--- plugins/trpc.ts
+++ plugins/trpc.ts
@@ -5,10 +5,10 @@ import superjson from 'superjson'
 export default defineNuxtPlugin(() => {
   // Get the base URL for API requests
   const config = useRuntimeConfig()
-  const baseUrl = import.meta.server 
+  const baseUrl = import.meta.server
     ? `http://${config.public.apiHost || 'localhost:3000'}`
     : ''
-  
+
   /**
    * Create tRPC client with properly formed URL
    */
