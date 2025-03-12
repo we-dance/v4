cfcdc5e63db2a88031978e8a71877ff85e801adb
Aleksey Razbakov <aleksey@razbakov.com>
2025-03-12 11:35:09 +0100

    register: add missing components

---
+++ components/forms/LoginForm.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps({
+  register: Boolean,
+})
+
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+import { toast } from 'vue-sonner'
+
+const { login } = useAppAuth()
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+const form = useForm({
+  validationSchema: toTypedSchema(schema),
+})
+
+async function onSubmit(values: Record<string, any>) {
+  const error = await login('credentials', values)
+
+  if (error) {
+    toast.error(error)
+  }
+}
+</script>
+
+<template>
+  <AutoForm
+    class="space-y-4"
+    :form="form"
+    :schema="schema"
+    :field-config="{
+      email: { inputProps: { type: 'email' } },
+      password: { inputProps: { type: 'password' } },
+    }"
+    @submit="onSubmit"
+  >
+    <TermsInfo />
+
+    <div class="flex justify-end">
+      <Button type="submit"> Login </Button>
+    </div>
+  </AutoForm>
+</template>
