e0eb518d835561076019f174bb0dacc1bf83d7d1
Aleksey Razbakov <aleksey@razbakov.com>
2025-03-13 10:31:05 +0100

    auth: fix terms

--- components/forms/TermsInfo.vue
+++ components/forms/TermsInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="prose text-sm">
+  <div class="prose text-sm mb-4">
     By signing in, you agree to
     <router-link to="/terms">Terms of service</router-link> and
     <router-link to="/privacy">Privacy policy</router-link>.
