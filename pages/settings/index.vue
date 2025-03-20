72c99f0bddbd00dc935188d108ea5e6fe094a9e4
Yannan Zhang <yannan.zhang@tuni.fi>
2025-03-20 11:57:15 +0200

    update setting page

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,10 +3,28 @@ definePageMeta({
   layout: 'settings',
   middleware: ['auth'],
 })
+
+const showCancellationDialog = ref(false)
+const selectedSubscription = ref<any>(null)
+
+const handleOpenCancelDialog = (subscription: any) => {
+  selectedSubscription.value = subscription
+  showCancellationDialog.value = true
+}
+
 </script>
 
 <template>
   <AccountInformation />
+  <SubscriptionSettings @open-cancel-dialog="handleOpenCancelDialog" />
   <PasswordSettings />
   <DangerZone />
+
+    <!-- Cancellation Dialog -->
+  <CancelSubscriptionDialog
+    :is-open="showCancellationDialog"
+    :subscription="selectedSubscription"
+    @close="showCancellationDialog = false"
+  />
+  <!-- @confirm="/* Cancellation logic will go here */" -->
 </template>
