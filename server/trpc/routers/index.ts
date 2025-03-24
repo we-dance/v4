31af4321726c160c082ad22d2e8f5e005014cd73
Yannan Zhang <yannan.zhang@tuni.fi>
2025-03-24 20:25:02 +0200

    subscription Router && apply endpoints in index.ts for routers && format

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -3,12 +3,14 @@ import { postsRouter } from './posts'
 import { eventsRouter } from './events'
 import { profilesRouter } from './profiles'
 import { usersRouter } from './users'
+import { subscriptionsRouter } from './subscriptions'
 
 export const appRouter = router({
   posts: postsRouter,
   events: eventsRouter,
   profiles: profilesRouter,
   users: usersRouter,
+  subscriptions: subscriptionsRouter,
 })
 
 export type AppRouter = typeof appRouter
