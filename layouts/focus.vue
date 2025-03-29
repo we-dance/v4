2ef418b9feabd35fd21eaf4b7a7e03f432c2c4c6
Mr. Robot <gomexxxx71@gmail.com>
2025-03-30 00:31:43 +0600

    chore: provide dark color mode

--- layouts/focus.vue
+++ layouts/focus.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div
+    class="min-h-screen flex flex-col dark:bg-[hsl(var(--dark))] dark:text-white"
+  >
     <main class="flex-1">
       <slot />
     </main>
