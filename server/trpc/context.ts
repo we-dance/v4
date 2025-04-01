3ea2f06c8c3e7f2c453e8ea28abdde53adee4f5e
Yannan Zhang <yannan.zhang@tuni.fi>
2025-04-01 21:54:25 +0300

    fix: modify stripe to adapt course data, generate payment url link and redirect to success.vue

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -4,6 +4,9 @@ import type { H3Event } from 'h3'
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export function createContext(event: H3Event) {
-  return { event }
+export function createContext(_event: H3Event) {
+  return {
+    prisma: _event.context.prisma,
+    event: _event,
+  }
 }
