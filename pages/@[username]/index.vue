063bae567a5eee3aada2d0ab18815bd460acdf3f
Aleksey Razbakov <aleksey@razbakov.com>
2025-04-07 01:01:54 +0200

    fix artists

--- pages/@[username]/index.vue
+++ pages/@[username]/index.vue
@@ -3,12 +3,12 @@ const { profile, isOwner } = await useProfile()
 </script>
 
 <template>
-  <ArtistLayout v-if="profile" :profile="profile">
+  <ProfileLayout v-if="profile" :profile="profile">
     <div class="max-w-xl mx-auto">
       <Create v-if="isOwner" class="mb-6" />
       <PostList :type="'all'" :limit="3" :author-id="profile.id" />
     </div>
-  </ArtistLayout>
+  </ProfileLayout>
 
   <EmptyState v-else variant="profile-not-found" />
 </template>
