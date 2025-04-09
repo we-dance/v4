4fe505160875db8ad4733b28435e96f8c607bb1b
Aleksey Razbakov <aleksey@razbakov.com>
2025-04-09 20:14:55 +0200

    fix prisma

--- server/trpc/routers/subscriptions.ts
+++ server/trpc/routers/subscriptions.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod'
 import { TRPCError } from '@trpc/server'
 import { publicProcedure, router } from '~/server/trpc/init'
+import { prisma } from '~/server/prisma'
 
 export const subscriptionsRouter = router({
   view: publicProcedure
     .input(z.object({ subscriptionId: z.string() }))
     .query(async ({ ctx, input }) => {
       const { subscriptionId } = input
-      const subscription = await ctx.prisma.subscription.findUnique({
+      const subscription = await prisma.subscription.findUnique({
         where: { id: subscriptionId },
         include: {
           offer: {
