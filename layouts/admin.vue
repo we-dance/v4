47a1146e23e6d9e89434da3d421bd661b9fad01d
Aleksey Razbakov <aleksey@razbakov.com>
2025-04-11 01:48:51 +0200

    admin layout

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -18,34 +18,32 @@ const items = [
   <div class="min-h-screen flex flex-col">
     <AppNavigation />
     <main class="flex-1 bg-background text-foreground">
-      <slot />
+      <SidebarProvider :defaultOpen="defaultOpen">
+        <Sidebar class="md:pt-16">
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Admin Area</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem v-for="item in items" :key="item.to">
+                    <SidebarMenuButton asChild>
+                      <NuxtLink :to="item.to">
+                        <Icon :name="item.icon" />
+                        <span>{{ item.label }}</span>
+                      </NuxtLink>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+        <SidebarInset>
+          <div class="flex flex-1 flex-col">
+            <slot />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </main>
-
-    <SidebarProvider :defaultOpen="defaultOpen">
-      <Sidebar class="md:pt-16">
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>Admin Area</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem v-for="item in items" :key="item.to">
-                  <SidebarMenuButton asChild>
-                    <NuxtLink :to="item.to">
-                      <Icon :name="item.icon" />
-                      <span>{{ item.label }}</span>
-                    </NuxtLink>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset>
-        <div class="flex flex-1 flex-col">
-          <slot />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
   </div>
 </template>
