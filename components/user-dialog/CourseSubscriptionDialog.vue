31afdbaa82af58050793227fbe9fac8144148aaf
Aleksey Razbakov <aleksey@razbakov.com>
2025-04-22 18:42:05 +0200

    fix subscription dialog on mobile

--- components/user-dialog/CourseSubscriptionDialog.vue
+++ components/user-dialog/CourseSubscriptionDialog.vue
@@ -33,32 +33,29 @@ const handleSelect = (offer: any) => {
   </DialogHeader>
 
   <div class="space-y-4 py-4">
-    <Button
+    <button
       v-for="offer in offers"
       :key="offer.id"
       variant="secondary"
-      class="w-full justify-between h-auto py-4 hover:border-accent"
+      class="flex w-full justify-between h-auto py-4 px-4 shadow-sm border rounded-md hover:border-accent hover:bg-muted hover:text-background-foreground text-sm font-medium"
       @click="handleSelect(offer)"
     >
-      <div class="flex items-center gap-3">
-        <div class="text-left flex-1">
+      <div class="flex flex-wrap items-center gap-3">
+        <div class="text-left">
           <div class="font-bold">{{ offer.name }}</div>
-          <ul class="mt-2 space-y-1">
+          <ul class="mt-2 text-sm text-muted-foreground">
             <li
               v-for="(item, index) in getItems(offer)"
               :key="index"
-              class="flex items-center gap-2 text-sm text-muted-foreground"
+              class="flex gap-x-2 gap-y-2"
             >
-              <Icon
-                name="ph:check"
-                class="w-3.5 h-3.5 text-accent flex-shrink-0"
-              />
+              <Icon name="ph:check" class="w-4 h-4 text-accent flex-none" />
               {{ item }}
             </li>
           </ul>
         </div>
       </div>
-      <div class="text-right">
+      <div class="text-right flex-none">
         <div class="font-bold">
           {{ formatCurrencyCents(offer.price, offer.currency) }}
         </div>
@@ -66,7 +63,7 @@ const handleSelect = (offer: any) => {
           {{ formatSubscriptionDuration(offer.duration) }}
         </div>
       </div>
-    </Button>
+    </button>
 
     <p class="text-xs text-center text-muted-foreground mt-4">
       All plans include 24-hours money-back guarantee. Cancel anytime.
