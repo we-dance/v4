55aa25bdae39ff82cfde7420388666207e8289da
Aleksey Razbakov <aleksey@razbakov.com>
2025-05-08 02:33:48 +0200

    create post

--- components/Create.vue
+++ components/Create.vue
@@ -1,3 +1,22 @@
+<script setup>
+const { session } = useAppAuth()
+
+const communities = ref([
+  { label: 'Salsa', value: 'salsa' },
+  { label: 'Bachata', value: 'bachata' },
+  { label: 'Kizomba', value: 'kizomba' },
+  { label: 'Zouk', value: 'zouk' },
+])
+const selectedCommunity = ref(null)
+
+const cities = ref([
+  { label: 'New York', value: 'new-york' },
+  { label: 'Los Angeles', value: 'los-angeles' },
+  { label: 'Chicago', value: 'chicago' },
+])
+const selectedCity = ref(null)
+</script>
+
 <template>
   <div
     class="flex items-center justify-between bg-primary/10 p-2 px-4 rounded-lg"
@@ -10,120 +29,146 @@
           Post
         </Button>
       </DialogTrigger>
-      <DialogContent class="sm:max-w-3xl max-h-[80vh] overflow-y-auto">
+      <DialogContent class="sm:max-w-xl max-h-[80vh] overflow-y-auto">
         <DialogHeader>
-          <DialogTitle>Build the Community Together</DialogTitle>
-          <DialogDescription
-            >Join other dancers in making the dance community even
-            better</DialogDescription
-          >
+          <DialogTitle></DialogTitle>
+          <div class="flex items-start gap-3">
+            <Avatar
+              :profile="session?.profile"
+              class="w-10 h-10 rounded-full object-cover hover:ring-2 hover:ring-primary transition-all"
+            />
+            <div class="flex-1">
+              <div
+                class="font-medium text-foreground hover:text-primary transition-colors"
+              >
+                {{ session?.profile?.name }}
+              </div>
+              <div class="flex items-center gap-4 text-sm">
+                <Combobox v-model="selectedCommunity" by="label">
+                  <ComboboxAnchor as-child>
+                    <ComboboxTrigger as-child>
+                      <Button
+                        variant="outline"
+                        size="skinny"
+                        class="w-auto text-xs"
+                      >
+                        {{ selectedCommunity?.label ?? 'Community' }}
+
+                        <Icon name="ph:caret-down" class="w-4 h-4" />
+                      </Button>
+                    </ComboboxTrigger>
+                  </ComboboxAnchor>
+
+                  <ComboboxList>
+                    <div class="relative w-full max-w-sm items-center">
+                      <ComboboxInput
+                        class="pl-9 focus-visible:ring-0 border-0 border-b rounded-none h-10"
+                        placeholder="Search community..."
+                      />
+                      <span
+                        class="absolute start-0 inset-y-0 flex items-center justify-center px-3"
+                      >
+                        <Search class="size-4 text-muted-foreground" />
+                      </span>
+                    </div>
+
+                    <ComboboxEmpty> No community found. </ComboboxEmpty>
+
+                    <ComboboxGroup>
+                      <ComboboxItem
+                        v-for="community in communities"
+                        :key="community.value"
+                        :value="community"
+                      >
+                        {{ community.label }}
+
+                        <ComboboxItemIndicator>
+                          <Icon
+                            name="ph:check"
+                            class="ml-auto h-4 w-4"
+                            :class="
+                              selectedCommunity?.value === community.value
+                                ? 'opacity-100'
+                                : 'opacity-0'
+                            "
+                          />
+                        </ComboboxItemIndicator>
+                      </ComboboxItem>
+                    </ComboboxGroup>
+                  </ComboboxList>
+                </Combobox>
+
+                <Combobox v-model="selectedCity" by="label">
+                  <ComboboxAnchor as-child>
+                    <ComboboxTrigger as-child>
+                      <Button
+                        variant="outline"
+                        size="skinny"
+                        class="w-auto text-xs"
+                      >
+                        {{ selectedCity?.label ?? 'City' }}
+
+                        <Icon name="ph:caret-down" class="w-4 h-4" />
+                      </Button>
+                    </ComboboxTrigger>
+                  </ComboboxAnchor>
+
+                  <ComboboxList>
+                    <div class="relative w-full max-w-sm items-center">
+                      <ComboboxInput
+                        class="pl-9 focus-visible:ring-0 border-0 border-b rounded-none h-10"
+                        placeholder="Search city..."
+                      />
+                      <span
+                        class="absolute start-0 inset-y-0 flex items-center justify-center px-3"
+                      >
+                        <Search class="size-4 text-muted-foreground" />
+                      </span>
+                    </div>
+
+                    <ComboboxEmpty> No community found. </ComboboxEmpty>
+
+                    <ComboboxGroup>
+                      <ComboboxItem
+                        v-for="city in cities"
+                        :key="city.value"
+                        :value="city"
+                      >
+                        {{ city.label }}
+
+                        <ComboboxItemIndicator>
+                          <Icon
+                            name="ph:check"
+                            class="ml-auto h-4 w-4"
+                            :class="
+                              selectedCity?.value === city.value
+                                ? 'opacity-100'
+                                : 'opacity-0'
+                            "
+                          />
+                        </ComboboxItemIndicator>
+                      </ComboboxItem>
+                    </ComboboxGroup>
+                  </ComboboxList>
+                </Combobox>
+              </div>
+            </div>
+          </div>
         </DialogHeader>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
-          <!-- Share -->
-          <div>
-            <div class="text-primary mb-4">
-              <Icon name="ph:pencil-line" class="w-8 h-8" />
-            </div>
-            <h3 class="font-bold text-lg mb-2 text-foreground">Share</h3>
-            <p class="text-muted-foreground mb-4">
-              Start conversations, share knowledge, and inspire others with your
-              dance journey
-            </p>
-            <div class="space-y-2">
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon
-                  name="ph:chat-centered-dots"
-                  class="w-5 h-5 text-primary mr-2"
-                />
-                <span>Thread</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon
-                  name="ph:article-medium"
-                  class="w-5 h-5 text-primary mr-2"
-                />
-                <span class="flex-1">Article</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon
-                  name="ph:youtube-logo"
-                  class="w-5 h-5 text-primary mr-2"
-                />
-                <span>Video</span>
-              </button>
-            </div>
-          </div>
-
-          <!-- Organize -->
-          <div>
-            <div class="text-primary mb-4">
-              <Icon name="ph:calendar-plus" class="w-8 h-8" />
-            </div>
-            <h3 class="font-bold text-lg mb-2 text-foreground">Organize</h3>
-            <p class="text-muted-foreground mb-4">
-              Create events, connect with dancers, and share opportunities
-            </p>
-            <div class="space-y-2">
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon
-                  name="ph:calendar-plus"
-                  class="w-5 h-5 text-primary mr-2"
-                />
-                <span>Event</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon name="ph:users" class="w-5 h-5 text-primary mr-2" />
-                <span>Meet</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon name="ph:briefcase" class="w-5 h-5 text-primary mr-2" />
-                <span>Gig</span>
-              </button>
-            </div>
-          </div>
-
-          <!-- Support -->
-          <div>
-            <div class="text-primary mb-4">
-              <Icon name="ph:handshake" class="w-8 h-8" />
-            </div>
-            <h3 class="font-bold text-lg mb-2 text-foreground">Support</h3>
-            <p class="text-muted-foreground mb-4">
-              Help others discover the local scene and dance community
-            </p>
-            <div class="space-y-2">
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon name="ph:star" class="w-5 h-5 text-primary mr-2" />
-                <span>Review</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon name="ph:info" class="w-5 h-5 text-primary mr-2" />
-                <span>Ask Locals</span>
-              </button>
-              <button
-                class="w-full text-left px-3 py-2 hover:bg-muted rounded-lg flex items-center text-foreground"
-              >
-                <Icon name="ph:storefront" class="w-5 h-5 text-primary mr-2" />
-                <span>Ad</span>
-              </button>
-            </div>
-          </div>
+        <Textarea
+          v-model="content"
+          placeholder="Share your dance story, ask questions, or post an event..."
+          class="resize-none text-base"
+        />
+        <div class="flex justify-start">
+          <Button size="icon" variant="ghost">
+            <Icon name="ph:image-square-bold" class="w-4 h-4" />
+          </Button>
+          <Button size="icon" variant="ghost">
+            <Icon name="ph:link-simple-bold" class="w-4 h-4" />
+          </Button>
+          <div class="flex-1"></div>
+          <Button variant="primary">Post</Button>
         </div>
       </DialogContent>
     </Dialog>
