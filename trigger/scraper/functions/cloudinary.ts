839e0cc2578a88be4e914ff107969e5d6db2698b
egemengunel <egemengunel@gmail.com>
2025-07-14 11:27:36 +0300

    Refactor cloudinary.ts to ensure environment variables use String()

--- trigger/scraper/functions/cloudinary.ts
+++ trigger/scraper/functions/cloudinary.ts
@@ -6,9 +6,9 @@ export async function getUploadedImage(url: string) {
   }
 
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
-    api_key: process.env.CLOUDINARY_API_KEY as string,
-    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+    cloud_name: String(process.env.CLOUDINARY_CLOUD_NAME),
+    api_key: String(process.env.CLOUDINARY_API_KEY),
+    api_secret: String(process.env.CLOUDINARY_API_SECRET),
   })
 
   const result = await cloudinary.uploader.upload(url)
