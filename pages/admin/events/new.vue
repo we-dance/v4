361f4690aec1f51ce5e30a9710cf40af5ca3fdbe
egemengunel <egemengunel@gmail.com>
2025-07-15 11:12:52 +0300

    uodate env to include TRIGGER_PROJECT_REF, import and new pages now use the admin page layout.

--- pages/admin/events/new.vue
+++ pages/admin/events/new.vue
@@ -1,4 +1,28 @@
+<script lang="ts">
+import SidebarTrigger from '~/components/ui/sidebar/SidebarTrigger.vue'
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['sidebase-auth'],
+})
+</script>
+
 <template>
+  <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+    <SidebarTrigger class="-ml-1" />
+    <Sepetator orientation="vertical" class="mr-2 h-4" />
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <NuxtLink to="/admin/events"> Manage Events </NuxtLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeperator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Add New Event</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  </header>
   <div class="min-h-screen flex items-center justify-center ml-8 mr-8">
     <AddEventForm />
   </div>
