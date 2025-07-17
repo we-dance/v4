4859e795395c5fa3a7cf3be93f839e377937cf86
egemengunel <egemengunel@gmail.com>
2025-07-17 16:11:14 +0300

    refactor: rename scrape.ts to import_event.ts, remove trigger/scraper/functions route

---
+++ trigger/import_event.ts
@@ -0,0 +1,10 @@
+import { logger, task } from '@trigger.dev/sdk/v3'
+import { importEvent } from '~/cli/importer/event/index'
+
+export const scrape = task({
+  id: 'import-event',
+  run: async (payload: any, { ctx }) => {
+    const { eventId, sourceUrl } = payload
+    await importEvent(eventId, sourceUrl)
+  },
+})
