84484588ab6a1924c87160e5ff20b48020819e32
egemengunel <egemengunel@gmail.com>
2025-07-21 14:42:28 +0300

    refactor: move database event handling into index.ts

--- cli/importer/event/index.ts
+++ cli/importer/event/index.ts
@@ -1,8 +1,50 @@
-import { saveEvent, handleImportFailure } from './manage_event'
 import axios from 'axios'
 import { getFacebookEvent } from './facebook_import'
 import { getSchemaEvent } from './schema_import'
 import { isFacebookEvent } from './linguist'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+//database operations
+async function saveEvent(eventId: string, eventDataForPrisma: any) {
+  try {
+    await prisma.event.update({
+      where: { id: eventId },
+      data: eventDataForPrisma,
+    })
+    console.log('Scrape was succesfull', { eventId })
+  } catch (error) {
+    console.error('Failed to update event in database', { eventId, error })
+    throw error
+  }
+}
+
+async function handleImportFailure(
+  eventId: string,
+  reason: string,
+  error?: any
+) {
+  console.error(reason, { eventId, error })
+  try {
+    const errorMessage = error ? `${reason}: ${String(error)}` : reason
+    await prisma.event.update({
+      where: { id: eventId },
+      data: {
+        name: 'Failed import',
+        slug: 'failed-import',
+        status: 'import_failed',
+        importError: errorMessage,
+      },
+    })
+    console.log('Marked event as import_failed', { eventId })
+  } catch (error) {
+    console.error('Failed to update event to import_failed.', {
+      eventId,
+      error: error,
+    })
+  }
+}
 
 async function getRedirectedUrl(url: string): Promise<string> {
   try {
