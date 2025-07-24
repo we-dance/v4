ac74d3cead945f7a5bacc411793707bbe02da4a8
Egemen Günel <egemengunel@gmail.com>
2025-07-24 13:58:11 +0300

    fix: remove manual import in index.ts

--- components/user-dialog/index.ts
+++ components/user-dialog/index.ts
@@ -1,36 +1,3 @@
-import PricingOptionsDialog from './PricingOptionsDialog.vue'
-import UserTypeInfoDialog from './UserTypeInfoDialog.vue'
-import ArtistBookingDialog from './ArtistBookingDialog.vue'
-import CourseSubscriptionDialog from './CourseSubscriptionDialog.vue'
-import VenueBookingDialog from './VenueBookingDialog.vue'
-import VenueAreaDetailsDialog from './VenueAreaDetailsDialog.vue'
-import SearchDialog from './SearchDialog.vue'
-import CourseReviewDialog from './CourseReviewDialog.vue'
-import CourseCreateDialog from './CourseCreateDialog.vue'
-import CourseOfferDialog from './CourseOfferDialog.vue'
-import CourseModuleDialog from './CourseModuleDialog.vue'
-import CourseLessonDialog from './CourseLessonDialog.vue'
-import CourseResourceDialog from './CourseResourceDialog.vue'
-import ReviewDialog from './ReviewDialog.vue'
-import EventCreateDialog from './EventCreateDialog.vue'
-
-export const dialogs = {
-  PricingOptionsDialog,
-  UserTypeInfoDialog,
-  ArtistBookingDialog,
-  CourseSubscriptionDialog,
-  VenueBookingDialog,
-  VenueAreaDetailsDialog,
-  SearchDialog,
-  CourseReviewDialog,
-  CourseCreateDialog,
-  CourseOfferDialog,
-  CourseModuleDialog,
-  CourseLessonDialog,
-  CourseResourceDialog,
-  ReviewDialog,
-  EventCreateDialog,
-}
 // Dynamically import all dialog Vue components
 const dialogModules = import.meta.glob('./*.vue', { eager: true })
 
