fb5433951035e99f71d300729688965d9ba42970
Egemen Günel <egemengunel@gmail.com>
2025-09-03 17:18:09 +0300

    feat: add inital imiplementation of Dance Style selection -add styles router to fetch styles rom database -update DanceStyleInput compoent to handle multiple style selection

--- components/inputs/DanceStyleInput.vue
+++ components/inputs/DanceStyleInput.vue
@@ -1,63 +1,97 @@
 <script setup lang="ts">
+import type { DanceStyle } from '@prisma/client'
+import { computed, ref } from 'vue'
+import { Check, X } from 'lucide-vue-next'
+import { cn } from '@/utils'
+
 const { $client } = useNuxtApp()
 
 const { data: allDanceStyles } = await $client.styles.list.useQuery()
 
 const props = defineProps<{
-  modelValue: string
+  modelValue: DanceStyle[]
 }>()
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: string): void
+  (e: 'update:modelValue', value: DanceStyle[]): void
 }>()
 
 const open = ref(false)
 const searchQuery = ref('')
 
-const value = computed({
-  get: () => props.modelValue,
+const filteredStyles = computed(() => {
+  if (!searchQuery.value) {
+    return allDanceStyles.value ?? []
+  }
+  return (allDanceStyles.value ?? []).filter((style) =>
+    style.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
+
+const selectedStyles = computed({
+  get: () => props.modelValue || [],
   set: (newValue) => emit('update:modelValue', newValue),
 })
 
-const onSelect = (newValue: any) => {
-  value.value = newValue.hashtag
-  open.value = false
+function toggleStyle(style: DanceStyle) {
+  const index = selectedStyles.value.findIndex((s) => s.id === style.id)
+  if (index > -1) {
+    const newSelection = [...selectedStyles.value]
+    newSelection.splice(index, 1)
+    selectedStyles.value = newSelection
+  } else {
+    selectedStyles.value = [...selectedStyles.value, style]
+  }
 }
 </script>
 
 <template>
-  <Popover v-model:open="open">
-    <PopoverTrigger as-child>
-      <Button
+  <div>
+    <div v-if="selectedStyles.length > 0" class="flex flex-wrap gap-2 mb-2">
+      <Badge
+        v-for="style in selectedStyles"
+        :ket="style.id"
         variant="secondary"
-        role="combobox"
-        :aria-expanded="open"
-        class="font-normal text-left"
+        class="flex items-center gap-x-1.5"
       >
-        <div class="flex-1">
-          <template v-if="value">
-            {{ allDanceStyles?.find((style) => style.hashtag === value)?.name }}
-          </template>
-          <template v-else>
-            <span class="text-muted-foreground">Dance style</span>
-          </template>
-        </div>
-        <Icon
-          name="heroicons:chevron-down"
-          class="h-4 w-4 shrink-0 ml-2 opacity-50"
+        {{ style.name }}
+        <button type="button" @click="toggleStyle(style)">
+          <X class="h-3 w-3" />
+        </button>
+      </Badge>
+    </div>
+    <Popover v-model:open="open">
+      <PopoverTrigger as-child>
+        <Button
+          variant="secondary"
+          role="combobox"
+          :aria-expanded="open"
+          class="font-normal text-left"
+        >
+          <span class="truncate-1">
+            {{
+              selectedStyles.length > 0
+                ? `${selectedStyles.length} selected`
+                : 'Select Dance Styles..'
+            }}
+          </span>
+          <Icon
+            name="heroicons:chevron-down"
+            class="h-4 w-4 shrink-0 ml-2 opacity-50"
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent class="p-0">
+        <SearchableDropdown
+          :items="filteredStyles"
+          :selection="selectedStyles"
+          v-model:searchQuery="searchQuery"
+          placeholder="Search dance style..."
+          itemKey="hashtag"
+          itemLabel="name"
+          @select="toggleStyle"
         />
-      </Button>
-    </PopoverTrigger>
-    <PopoverContent class="p-0">
-      <SearchableDropdown
-        :items="allDanceStyles ?? []"
-        v-model="value"
-        v-model:searchQuery="searchQuery"
-        placeholder="Search dance style..."
-        itemKey="hashtag"
-        itemLabel="name"
-        @select="onSelect"
-      />
-    </PopoverContent>
-  </Popover>
+      </PopoverContent>
+    </Popover>
+  </div>
 </template>
---
+++ server/trpc/routers/styles.ts
@@ -0,0 +1,13 @@
+import { publicProcedure, router } from '../init'
+import { prisma } from '~/server/prisma'
+
+export const stylesRouter = router({
+  list: publicProcedure.query(async () => {
+    const styles = await prisma.danceStyle.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    })
+    return styles
+  }),
+})
